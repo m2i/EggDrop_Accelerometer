8a13e428b75fa02212aa8610dbe60fbd731e1bc8
Matthew Nelson <kb0mgq@gmail.com>
2016-08-28 13:53:05 -0500

    Revision 1 code
    Working code, compiles and runs as expected on Feather board.

--- 3-Firmware/Feather_FW/Feather_FW.ino
+++ 3-Firmware/Feather_FW/Feather_FW.ino
@@ -21,6 +21,13 @@
  *  4GB SD Card
  */
 
+ /* Error codes
+  *  2 blinks - No SD Card present
+  *  3 blinks - No LIS3DH sensor present
+  *  4 blinks - Battery below 3.3 VDC
+  *  5 blinks - Could not create file on SD Card
+  */
+
 // include the SD library:
 #include <SPI.h>
 #include <SD.h>
@@ -29,11 +36,6 @@
 #include <Wire.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
-
-// set up variables using the SD utility library functions:
-Sd2Card card;
-SdVolume volume;
-SdFile root;
 
 // Set CS pin, for Feather this is Pin 4
 const int chipSelect = 4;
@@ -48,7 +50,36 @@ const int chipSelect = 4;
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
-File eggdrop;
+File logfile;
+
+//Functions
+//This reads the battery voltage on the feather
+float battery(void) {
+   
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  Serial.print("VBat: " ); Serial.println(measuredvbat);
+  return measuredvbat;
+}
+
+// blink out an error code
+void error(uint8_t errno) {
+  while(1) {
+    uint8_t i;
+    for (i=0; i<errno; i++) {
+      digitalWrite(LED1, HIGH);
+      delay(300);
+      digitalWrite(LED1, LOW);
+      delay(300);
+    }
+    for (i=errno; i<10; i++) {
+      delay(1000);
+    }
+  }
+}
+
 
 void setup() {
   // initialize digital pin 13 as an output.
@@ -65,33 +96,38 @@ void setup() {
 
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+  if (!SD.begin(chipSelect)) {
     //This flashes the error LED 2 times
+    Serial.println("Card init. failed!");
     error(2);
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    return;
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
   }
 
-  // print the type of card
-  Serial.print("\nCard type: ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
+  // create a new file
+  char filename[15];
+  strcpy(filename, "ANALOG00.TXT");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = '0' + i/10;
+    filename[7] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
       break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
+    }
   }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if( ! logfile ) {
+    Serial.print("Couldnt create "); 
+    Serial.println(filename);
+    error(5);
+  }
+  Serial.print("Writing to "); 
+  Serial.println(filename);
+
+  //Now let's print a header to identify the columns
+  logfile.println("Acceleration is in m/s^2");
+  logfile.println("Accel_X,ACCEL_Y,ACCEL_Z,Voltage");
+  logfile.flush();
+  
 
   Serial.println("Testing LIS3DH Sensor.....");
   
@@ -117,67 +153,38 @@ void setup() {
     Serial.println("BATTERY LOW!  RECHARGE!");
     error(4);
   }
-
 }
 
-float battery(void) {
-   
-  float measuredvbat = analogRead(VBATPIN);
-  measuredvbat *= 2;    // we divided by 2, so multiply back
-  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-  measuredvbat /= 1024; // convert to voltage
-  Serial.print("VBat: " ); Serial.println(measuredvbat);
-  return measuredvbat;
-}
-
-// blink out an error code
-void error(uint8_t errno) {
-  while(1) {
-    uint8_t i;
-    for (i=0; i<errno; i++) {
-      digitalWrite(LED1, HIGH);
-      delay(100);
-      digitalWrite(LED1, LOW);
-      delay(100);
-    }
-    for (i=errno; i<10; i++) {
-      delay(200);
-    }
-  }
-}
-
-
-
+//Main Loop
 void loop() {
+  float eggbreak;
   //Turn on LED2
   digitalWrite(LED2, HIGH);
   //Read the acceldata and battery voltage, create a data string
   //Data string will have the follwoing format
   //AccelX,AccelY,AccelZ,Battery
   //m/s^2,m/s^2,m/s^2,voltage
-  String dataString = "";
-  dataString = String(lis.x) + "," + String(lis.y) + "," + String(lis.z) + "," + String(battery());
-
-  //Now write the data to the SD Card
-  File dataFile = SD.open("eggdrop.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataString);
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
-  }
+  sensors_event_t event;
+  lis.getEvent(&event);
   
-  // Read the battery voltage
-  //battery();
+  logfile.print(event.acceleration.x);
+  logfile.print(",");
+  logfile.print(event.acceleration.y);
+  logfile.print(",");
+  logfile.print(event.acceleration.z);
+  logfile.print(",");
+  logfile.println(battery());
+  logfile.flush();
+
+  eggbreak = sqrt((event.acceleration.x*event.acceleration.x)+(event.acceleration.y*event.acceleration.y)+(event.acceleration.z+event.acceleration.z));
+  if (eggbreak > 16) {
+    digitalWrite(LED1,HIGH);
+    logfile.println("EGGBREAK,EGGBREAK,EGGBREAK,EGGBREAK");
+    logfile.flush();
+  }
   //Done, turn off LED
   digitalWrite(LED2, LOW);
-  delay(1000);
+  delay(200);
   
 
 }
